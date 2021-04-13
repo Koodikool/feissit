fe14dd8f2cc94afd784c41606c0b94bc0195ca4d
Krister Viirsaar <git@krister.ee>
2021-04-13 13:18:09 +0300

    Kui nupule klikitakse, siis logi konsooli teade

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 console.log('See käsk kuvab konsooli teate. Otsi mind üles!')
 console.log('Vihje Googeldamiseks: Developer Tools Console')
+
+document.querySelector('#regamise-nupp').onclick = function(event) {
+    console.log('Nupule klikiti!')
+}
